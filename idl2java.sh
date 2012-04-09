3349529b43ef0b8d70c1d84b143567e97264a6d2
James Cooper <james@bitmechanic.com>
2012-04-08 21:31:39 -0700

    add VERSION variable

--- idl2java.sh
+++ idl2java.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-CLASSPATH=$HOME/.m2/repository/com/bitmechanic/barrister/1.0-SNAPSHOT/barrister-1.0-SNAPSHOT.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-mapper-asl/1.9.4/jackson-mapper-asl-1.9.4.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-core-asl/1.9.4/jackson-core-asl-1.9.4.jar:$CLASSPATH
+VERSION=0.1.0-SNAPSHOT
+
+CLASSPATH=$HOME/.m2/repository/com/bitmechanic/barrister/$VERSION/barrister-$VERSION.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-mapper-asl/1.9.4/jackson-mapper-asl-1.9.4.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-core-asl/1.9.4/jackson-core-asl-1.9.4.jar:$CLASSPATH
 
 MAIN=com.bitmechanic.barrister.Idl2Java
 
