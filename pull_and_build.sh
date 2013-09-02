00a9bedb3c8a5022ba816a400fcfe4097743b485
James Cooper <james@bitmechanic.com>
2013-09-02 14:55:11 -0700

    rsync from main dir, not conform

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -9,7 +9,7 @@ git pull
 mvn clean compile
 ./conform_idl2java.sh
 mvn install
+rsync -avz target/site/javadoc/apidocs/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/java/latest/
 
 cd conform
 mvn clean package
-rsync -avz target/site/javadoc/apidocs/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/java/latest/
