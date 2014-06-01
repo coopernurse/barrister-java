26779750e8024d8bbfcf29a52bef32e31942073b
James Cooper <jamespcooper@gmail.com>
2014-05-31 22:16:59 -0700

    pull_and_build.sh: remove JAVA_HOME/PATH. comment out rsync

--- pull_and_build.sh
+++ pull_and_build.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-export JAVA_HOME=/usr/local/java
-export PATH=$JAVA_HOME/bin:/usr/local/maven/bin:$PATH
-
 git pull
 mvn clean compile
 ./conform_idl2java.sh
 mvn install
-rsync -avz target/site/javadoc/apidocs/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/java/latest/
+#rsync -avz target/site/javadoc/apidocs/ james@barrister.bitmechanic.com:/home/james/barrister-site/api/java/latest/
 
 cd conform
 mvn clean package
