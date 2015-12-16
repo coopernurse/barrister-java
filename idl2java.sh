82655140e01108a92aa549e5b83b1af122bb74b9
James Cooper <james@bitmechanic.com>
2015-12-16 09:35:53 -0800

    bump v0.1.15

--- idl2java.sh
+++ idl2java.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=0.1.14
+VERSION=0.1.15
 
 CLASSPATH=$HOME/.m2/repository/com/bitmechanic/barrister/$VERSION/barrister-$VERSION.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-mapper-asl/1.9.4/jackson-mapper-asl-1.9.4.jar:$HOME/.m2/repository/org/codehaus/jackson/jackson-core-asl/1.9.4/jackson-core-asl-1.9.4.jar:$CLASSPATH
 
